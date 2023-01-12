49adb3f0d5b2cecc9f86d6ba1e27bcf0f737d801
PALOMA AZEVEDO SANTOS <paloma.asantos17@senacsp.edu.br>
2023-01-11 21:29:37 -0300

    Adicionar arquivos de projeto.

---
+++ ConsultaCepXamarin/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ConsultaCepXamarin.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ConsultaCepXamarin.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
